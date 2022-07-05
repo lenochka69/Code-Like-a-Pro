7f480fdf387faeaad9bc1b84dd18da644ea7b7e1
lenochka69 <93248482+lenochka69@users.noreply.github.com>
2022-07-05 17:30:35 +0300

    like_share

--- app/src/main/java/com/example/codelikeapro/MainActivity.kt
+++ app/src/main/java/com/example/codelikeapro/MainActivity.kt
@@ -3,9 +3,53 @@ package com.example.codelikeapro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.viewModels
+
+import com.example.codelikeapro.databinding.ActivityMainBinding
+
+
+private const val TAG = "MainActivity"
+private const val MY_FILTER_TAG = "myfilter"
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel by viewModels<PostViewModel>()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                textLink.text = post.link
+
+                val likeImage = if (post.likeByMe) {
+                    R.drawable.ic_favorite_24
+                } else {
+                    R.drawable.favorite
+                }
+                favorite?.setImageResource(likeImage)
+                like?.text = Utils.numToPostfix(post.likes)
+                repost?.text = Utils.numToPostfix(post.reposted)
+            }
+
+        }
+        binding.favorite?.setOnClickListener {
+            viewModel.like()
+        }
+
+        binding.share?.setOnClickListener {
+            viewModel.repost()
+
+        }
+        Log.d(MY_FILTER_TAG, "onCreate()")
+
     }
 }
+
