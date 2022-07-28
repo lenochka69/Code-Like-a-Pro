4e1ef696e30faf6e95c5e384c081ecda7d11d43e
lenochka69 <93248482+lenochka69@users.noreply.github.com>
2022-07-28 23:12:56 +0300

    CrudTwo

--- app/src/main/java/com/example/codelikeapro/MainActivity.kt
+++ app/src/main/java/com/example/codelikeapro/MainActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import android.provider.Contacts.SettingsColumns.KEY
 
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -17,6 +14,8 @@ import com.example.codelikeapro.databinding.ActivityMainBinding
 import com.example.codelikeapro.databinding.CardPostBinding
 
 
+private const val TAG = "MainActivity"
+private const val MY_FILTER_TAG = "myfilter"
 
 private const val TAG = "MainActivity"
 private const val MY_FILTER_TAG = "myfilter"
@@ -29,75 +28,17 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val viewModel: PostViewModel by viewModels()
-
-        binding.group.visibility = View.GONE
-
-        val adapter = PostAdapter(
-            object : OnInteractionListeren {
-                override fun onEdit(post: Post) {
-                    viewModel.edit(post)
-                    binding.group.visibility = View.VISIBLE
-                }
-
-                override fun onRemove(post: Post) {
-                    viewModel.removeById(post.id)
-                }
-
-                override fun onLike(post: Post) {
-                    viewModel.likeById(post.id)
-                }
-
-                override fun onShare(post: Post) {
-                    viewModel.shareById(post.id)
-                }
-            }
+        val adapter = PostsAdapter(
+            onLikeListener = { viewModel.likeById(it.id) },
+            onShareListener = { viewModel.repostById(it.id) }
         )
-        binding.container.adapter = adapter
-
+        binding.list.adapter = adapter
 
         viewModel.data.observe(this) { posts ->
-            val newPost = adapter.itemCount < posts.size
-            adapter.submitList(posts) {
-                if (newPost) binding.container.smoothScrollToPosition(0)
-            }
+            adapter.submitList(posts)
         }
-
-        viewModel.edited.observe(this) {
-            with (binding.contentCheck) {
-                text = it.content
-            }
-            binding.content.setText(it.content)
-        }
-
-        binding.save.setOnClickListener {
-            with(binding.content) {
-                if (text.isNullOrBlank()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        context.getString(R.string.edit_message),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-                viewModel.changeContent(text.toString())
-                viewModel.save()
-                setText("")
-                clearFocus()
-                Utils.hideKeyboard(this)
-                binding.group.visibility = View.GONE
-            }
-        }
-
-        binding.cancel.setOnClickListener {
-            with(binding.content) {
-                setText("")
-                clearFocus()
-                Utils.hideKeyboard(this)
-                binding.group.visibility = View.GONE
-            }
-        }
+        Log.d(MY_FILTER_TAG, "onCreate()")
     }
-
 }
 
 
--- app/src/main/java/com/example/codelikeapro/PostRepositoryImpl.kt
+++ app/src/main/java/com/example/codelikeapro/PostRepositoryImpl.kt
@@ -17,6 +17,7 @@ class PostRepositoryImpl: PostRepository {
         id = 1,
         likeByMe = false,
         likes = 9999,
+
             reposted = 900
             ),
         Post (
@@ -27,46 +28,7 @@ class PostRepositoryImpl: PostRepository {
             id = 2,
             likeByMe = false,
             likes = 9999,
-            reposted = 900
-        ),
-        Post (
-            author = "Неотология. Университет интернет - профессий.",
-            published = "26 июня 2022 года",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам от новичков до уверенных профессионалов.",
-            link = "Но самое важное остается с нами: мыверим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен ->  http:/netolo.gy/fyb",
-            id = 3,
-            likeByMe = false,
-            likes = 9999,
-            reposted = 900
-        ),
-        Post (
-            author = "Неотология. Университет интернет - профессий.",
-            published = "26 июня 2022 года",
-            content = "Привет, это новая Нетология!",
-            link = "Наша миссия - помочь встать на путь роста и начать цепочку перемен ->  http:/netolo.gy/fyb",
-            id = 4,
-            likeByMe = false,
-            likes = 9999,
-            reposted = 900
-        ),
-        Post (
-            author = "Неотология. Университет интернет - профессий.",
-            published = "26 июня 2022 года",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам от новичков до уверенных профессионалов.",
-            link = "Но самое важное остается с нами: мыверим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен ->  http:/netolo.gy/fyb",
-            id = 5,
-            likeByMe = false,
-            likes = 9999,
-            reposted = 900
-        ),
-        Post (
-            author = "Неотология. Университет интернет - профессий.",
-            published = "26 июня 2022 года",
-            content = "Привет, это новая Нетология!",
-            link = "Наша миссия - помочь встать на путь роста и начать цепочку перемен ->  http:/netolo.gy/fyb",
-            id = 6,
-            likeByMe = false,
-            likes = 9999,
+
             reposted = 900
         ),
     )
@@ -88,26 +50,4 @@ class PostRepositoryImpl: PostRepository {
         data.value = posts
     }
 
-    override fun removeById (id: Long) {
-        posts =posts.filterNot { it.id == id
-
-        }
-        data.value = posts
-    }
-
-    override fun save(post: Post) {
-        posts = if (post.id == 0L) {
-                listOf(
-                    post.copy(
-                        id = post.id + 1L
-                    )
-                ) + posts
-        } else {
-                posts.map {
-                    if (it.id != post.id) it else it.copy(content = post.content)
-                }
-            }
-            data.value = posts
-        }
-    }
-
+}
